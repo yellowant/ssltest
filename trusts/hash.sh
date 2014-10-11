b71a558100ec614d7855e4d33ce1f65628ecf4d2
Felix Dörre <felix@dogcraft.de>
2014-10-11 19:00:33 +0200

    adding openbsd 5.5 truststore

--- trusts/hash.sh
+++ trusts/hash.sh
@@ -1,9 +1,10 @@
-for i in *.pem *.crt; do
+for i in $(find . -name "*.pem" -or -name "*.crt"); do
     csplit -z "$i" -f "$i." -b "%d.crt" "/^-----BEGIN CERTIFICATE--/" "{*}" >/dev/null
     rm $i
 done
 for i in *.crt; do
     name=$(openssl x509 -in "$i" -noout -fingerprint -sha512 | sed "s/.*=//" | tr -d ":")
     echo $name
-    mv "$i" "$name.crt"
+    openssl x509 -in "$i" -out "$name.crt"
+    rm $i
 done
