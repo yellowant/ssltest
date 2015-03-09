9fc86a503ee5f2aa5481acc9c9279d2013a09da0
Benny Baumann <BenBE@geshi.org>
2015-03-09 22:48:48 +0100

    fix: Properly escape JSON

--- src/de/dogcraft/ssltest/utils/JSONUtils.java
+++ src/de/dogcraft/ssltest/utils/JSONUtils.java
@@ -3,6 +3,9 @@ package de.dogcraft.ssltest.utils;
 public class JSONUtils {
 
     public static String jsonEscape(String s) {
-        return s.replace("\\", "\\\\").replace("\"", "\\\"");
+
+        return s.replaceAll("\\\\", "\\\\\\\\").replaceAll("\"", "\\\"").replaceAll("\\n", "\\\\n").replaceAll("\\r", "\\\\r").replaceAll("\\t", "\\\\t").replaceAll("\\b", "\\\\b").replaceAll("\\f", "\\\\f");
+
     }
+
 }
