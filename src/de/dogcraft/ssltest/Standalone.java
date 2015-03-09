0434f7fbe2ced108a0f961573618972be967edd0
Felix Dörre <felix@dogcraft.de>
2015-03-09 22:45:51 +0100

    add: react on "X-Forwarded-*"-Headers

--- src/de/dogcraft/ssltest/Standalone.java
+++ src/de/dogcraft/ssltest/Standalone.java
@@ -1,7 +1,11 @@
 package de.dogcraft.ssltest;
 
+import org.eclipse.jetty.server.ForwardedRequestCustomizer;
 import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
@@ -14,7 +18,13 @@ import de.dogcraft.ssltest.service.TruststoreOverview;
 public class Standalone {
 
     public static void main(String[] args) throws Exception {
-        Server s = new Server(8080);
+        Server s = new Server();
+        HttpConfiguration hc = new HttpConfiguration();
+        hc.addCustomizer(new ForwardedRequestCustomizer());
+        ServerConnector sc = new ServerConnector(s, new HttpConnectionFactory(hc));
+        sc.setPort(8080);
+        s.addConnector(sc);
+
         ServletContextHandler main = new ServletContextHandler();
         main.addServlet(new ServletHolder(new Service()), "/*");
         main.addServlet(new ServletHolder(new TruststoreOverview()), "/trust");
