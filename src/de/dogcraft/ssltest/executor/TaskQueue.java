d22b6928bec694afed3a923b505ed38fe1eb2c1b
Benny Baumann <BenBE@geshi.org>
2015-03-14 13:47:10 +0100

    add: Simple dependency-based task executor

---
+++ src/de/dogcraft/ssltest/executor/TaskQueue.java
@@ -0,0 +1,167 @@
+package de.dogcraft.ssltest.executor;
+
+import java.lang.Thread.State;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class TaskQueue {
+
+    public static void main(String[] args) {
+        final TaskQueue q = new TaskQueue();
+
+        Task t1 = new Task() {
+
+            @Override
+            public void run() {
+                System.out.println("Hallo Welt!");
+
+                Task t2 = new Task() {
+
+                    @Override
+                    public void run() {
+                        System.out.println("Task that we just created ...");
+                    }
+
+                };
+
+                Task t3 = new Task() {
+
+                    @Override
+                    public void run() {
+                        System.out.println("This is Task 3");
+                    }
+
+                };
+
+                Task t4 = new Task() {
+
+                    @Override
+                    public void run() {
+                        System.out.println("And finally Task 4");
+                    }
+
+                };
+
+                t4.dependsOn.add(t3);
+                t4.dependsOn.add(t2);
+
+                t2.dependsOn.add(t3);
+
+                q.addTask(t2);
+                q.addTask(t3);
+                q.addTask(t4);
+            }
+
+        };
+
+        q.addTask(t1);
+
+        q.start();
+
+        while ( !q.isCompleted()) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    protected synchronized void addTask(Task task) {
+        tasks.add(task);
+        if (isRunning()) {
+            this.start();
+        }
+    }
+
+    private final PriorityQueue<Task> tasks = new PriorityQueue<Task>();
+
+    private Thread taskThread = null;
+
+    private boolean running = false;
+
+    public boolean isRunning() {
+        return this.running;
+    }
+
+    public boolean isCompleted() {
+        return isRunning() && tasks.isEmpty() && (null == taskThread || State.TERMINATED == taskThread.getState());
+    }
+
+    private synchronized void start() {
+        running = true;
+
+        if (null != taskThread) {
+            if ( !taskThread.isAlive()) {
+                taskThread = null;
+            }
+        }
+
+        if (null == taskThread) {
+            taskThread = new Thread() {
+
+                @Override
+                public void run() {
+                    super.run();
+
+                    synchronized (TaskQueue.this) {
+                        while ( !TaskQueue.this.tasks.isEmpty()) {
+                            Task t = TaskQueue.this.tasks.poll();
+
+                            if (null == t) {
+                                break;
+                            }
+
+                            t.run();
+                        }
+                    }
+                }
+            };
+        }
+
+        if (State.NEW == taskThread.getState()) {
+            taskThread.start();
+        }
+    }
+
+    public static abstract class Task implements Runnable, Comparable<Task> {
+
+        private List<Task> dependsOn = new LinkedList<Task>();
+
+        public boolean isBlocked() {
+            if (dependsOn.isEmpty()) {
+                return false;
+            }
+
+            for (Task dependentTask : dependsOn) {
+                dependentTask.isCompleted();
+            }
+
+            return false;
+        }
+
+        public boolean isCompleted() {
+            return false;
+        }
+
+        @Override
+        public int compareTo(Task other) {
+            if (this.isBlocked() && !other.isBlocked()) {
+                return -1;
+            } else if ( !this.isBlocked() && other.isBlocked()) {
+                return 1;
+            }
+
+            if (this.dependsOn.contains(other)) {
+                return 1;
+            } else if (other.dependsOn.contains(this)) {
+                return -1;
+            }
+
+            return this.dependsOn.size() - other.dependsOn.size();
+        }
+
+    }
+
+}
