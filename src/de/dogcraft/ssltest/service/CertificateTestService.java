1f2ce3ee66d87dc3bbb8f08fea5ef3cd93a31f95
Benny Baumann <BenBE@geshi.org>
2016-01-09 13:39:18 +0100

    fix: Cache the regexp for fingerprint matching

--- src/de/dogcraft/ssltest/service/CertificateTestService.java
+++ src/de/dogcraft/ssltest/service/CertificateTestService.java
@@ -3,6 +3,8 @@ package de.dogcraft.ssltest.service;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,16 +17,22 @@ public class CertificateTestService extends TestService {
 
     private static final HashMap<String, CertificateWrapper> cache = new HashMap<>();
 
+    private static final Pattern fpPattern = Pattern.compile("[0-9a-f]{128}", Pattern.CASE_INSENSITIVE);
+
     @SuppressWarnings("deprecation")
     public void performTest(HttpServletRequest req, HttpServletResponse resp, boolean useEventStream) throws IOException {
         super.performTest(req, resp, useEventStream);
 
         String fp = req.getParameter("fp");
+
         fp = fp.toLowerCase();
-        if ( !fp.matches("[a-f0-9]+")) {
-            resp.sendError(404);
+        Matcher fpMatcher = fpPattern.matcher(fp);
+
+        if ( !fpMatcher.matches()) {
+            resp.setStatus(400, "Invalid fingerprint format. Please use SHA-512 fingerprint to continue.");
             return;
         }
+
         CertificateWrapper c = cache.get(fp);
         if (c == null) {
             resp.sendError(404);
@@ -49,14 +57,16 @@ public class CertificateTestService extends TestService {
                     observingOnly = true;
                 }
             }
+
             to.attach(ps);
+
             if (observingOnly) {
                 to.waitForCompletion();
                 return;
             }
         }
-        to.performTest();
 
+        to.performTest();
     }
 
     public static void cache(CertificateWrapper wrap) {
