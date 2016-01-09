0455b648035aae0ea61f611c66d5171fcfaff7da
Benny Baumann <BenBE@geshi.org>
2016-01-09 11:42:33 +0100

    chg: Refactor the Test Service stuff into its own class for extensibility

--- src/de/dogcraft/ssltest/service/Service.java
+++ src/de/dogcraft/ssltest/service/Service.java
@@ -81,120 +81,137 @@ public class Service extends HttpServlet {
         }
     }
 
-    HashMap<String, List<String>> cacheHostIPs = new HashMap<>();
-
-    HashMap<String, TestingSession> cacheTestSession = new HashMap<>();
-
-    @SuppressWarnings("deprecation")
     private void reqTestServer(HttpServletRequest req, HttpServletResponse resp, boolean useEventStream) throws IOException {
-        if (useEventStream) {
-            resp.setContentType("text/event-stream");
-        } else {
-            resp.setContentType("text/plain");
-        }
+        ServerTestService sts = new ServerTestService();
+        sts.performTest(req, resp, useEventStream);
+    }
 
-        TestParameter u;
-        try {
-            u = URLParsing.parse(req);
-            String port = u.getProtocol() + "-" + Integer.toString(u.getPort());
-            if ( !u.getHost().equals(req.getParameter("domain")) || !port.equals(req.getParameter("port"))) {
-                // Input has been canonicalized, send redirect.
-                String ip = req.getParameter("ip");
-                String url = req.getPathInfo() + "?domain=" + URLEncoder.encode(u.getHost(), "UTF-8")//
-                        + "&port=" + URLEncoder.encode(port, "UTF-8")//
-                        + (ip != null ? "&ip" + URLEncoder.encode(ip, "UTF-8") : "");
+    private static class TestService {
 
-                resp.sendRedirect(url);
-                return;
+        public void performTest(HttpServletRequest req, HttpServletResponse resp, boolean useEventStream) throws IOException {
+            if (useEventStream) {
+                resp.setContentType("text/event-stream");
+            } else {
+                resp.setContentType("text/plain");
             }
-        } catch (TestParameterParsingException e) {
-            resp.sendError(404, e.getMessage());
-            return;
         }
 
-        List<String> iplist;
+    }
 
-        synchronized (cacheHostIPs) {
-            iplist = cacheHostIPs.get(u.getHost());
-            if (iplist == null) {
-                iplist = new ArrayList<String>();
-                try {
-                    InetAddress[] addrlist = InetAddress.getAllByName(u.getHost());
-                    for (InetAddress addr : addrlist) {
-                        iplist.add(addr.getHostAddress());
-                    }
-                } catch (UnknownHostException e) {
-                    resp.sendError(404, e.getMessage());
+    private static class ServerTestService extends TestService {
+
+        protected static HashMap<String, List<String>> cacheHostIPs = new HashMap<>();
+
+        protected static HashMap<String, TestingSession> cacheTestSession = new HashMap<>();
+
+        @SuppressWarnings("deprecation")
+        public void performTest(HttpServletRequest req, HttpServletResponse resp, boolean useEventStream) throws IOException {
+            super.performTest(req, resp, useEventStream);
+
+            TestParameter u;
+            try {
+                u = URLParsing.parse(req);
+                String port = u.getProtocol() + "-" + Integer.toString(u.getPort());
+                if ( !u.getHost().equals(req.getParameter("domain")) || !port.equals(req.getParameter("port"))) {
+                    // Input has been canonicalized, send redirect.
+                    String ip = req.getParameter("ip");
+                    String url = req.getPathInfo() + "?domain=" + URLEncoder.encode(u.getHost(), "UTF-8")//
+                            + "&port=" + URLEncoder.encode(port, "UTF-8")//
+                            + (ip != null ? "&ip" + URLEncoder.encode(ip, "UTF-8") : "");
+
+                    resp.sendRedirect(url);
                     return;
                 }
-                cacheHostIPs.put(u.getHost(), iplist);
-            }
-        }
-
-        String ip = req.getParameter("ip");
-        if ((ip != null) && !iplist.contains(ip)) {
-            resp.setStatus(404, "Host not found at this location");
-        }
-
-        PrintStream ps = new PrintStream(resp.getOutputStream(), true);
-        ps.println("retry: 10000");
-        ps.println();
-
-        if (null == ip) {
-            ps.println("event: streamID");
-            ps.println("data: {\"host\":\"" + JSONUtils.jsonEscape(u.getHost()) + "\", "//
-                    + "\"port\":" + u.getPort() + ", \"proto\":\"" + JSONUtils.jsonEscape(u.getProtocol()) + "\"}");
-            ps.println();
-            for (String hostip : iplist) {
-                ps.println("event: hostip");
-                ps.println("data: {");
-                ps.println("data: \"domain\": \"" + JSONUtils.jsonEscape(u.getHost()) + "\",");
-                ps.println("data: \"port\": \"" + JSONUtils.jsonEscape(u.getProtocol()) + "-" + JSONUtils.jsonEscape(Integer.toString(u.getPort())) + "\",");
-                ps.println("data: \"ip\": \"" + JSONUtils.jsonEscape(hostip) + "\"");
-                ps.println("data: }");
-                ps.println();
+            } catch (TestParameterParsingException e) {
+                resp.sendError(404, e.getMessage());
+                return;
             }
 
-            ps.println("event: eof");
-            ps.println("data: {");
-            ps.println("data: msg: \"IP lookup completed.\"");
-            ps.println("data: }");
-            ps.println();
-            return;
-        } else if ( !iplist.contains(ip)) {
-            ps.println("event: eof");
-            ps.println("data: {");
-            ps.println("data: msg: \"Host not found at this address.\"");
-            ps.println("data: }");
-            ps.println();
-            return;
-        }
+            List<String> iplist;
 
-        TestingSession to;
-        {
-            boolean observingOnly = false;
-
-            synchronized (cacheTestSession) {
-                String lookupKey = ip + "#" + u.getHost() + ":" + u.getProtocol() + "-" + u.getPort();
-
-                to = cacheTestSession.get(lookupKey);
-                if (to == null) {
-                    to = new TestingSession(u.getHost(), ip, u.getPort(), u.getProtocol());
-                    cacheTestSession.put(lookupKey, to);
-                } else {
-                    observingOnly = true;
+            synchronized (cacheHostIPs) {
+                iplist = cacheHostIPs.get(u.getHost());
+                if (iplist == null) {
+                    iplist = new ArrayList<String>();
+                    try {
+                        InetAddress[] addrlist = InetAddress.getAllByName(u.getHost());
+                        for (InetAddress addr : addrlist) {
+                            iplist.add(addr.getHostAddress());
+                        }
+                    } catch (UnknownHostException e) {
+                        resp.sendError(404, e.getMessage());
+                        return;
+                    }
+                    cacheHostIPs.put(u.getHost(), iplist);
                 }
             }
 
-            to.attach(ps);
+            String ip = req.getParameter("ip");
+            if ((ip != null) && !iplist.contains(ip)) {
+                resp.setStatus(404, "Host not found at this location");
+            }
 
-            if (observingOnly) {
-                to.waitForCompletion();
+            PrintStream ps = new PrintStream(resp.getOutputStream(), true);
+            ps.println("retry: 10000");
+            ps.println();
+
+            if (null == ip) {
+                ps.println("event: streamID");
+                ps.println("data: {\"host\":\"" + JSONUtils.jsonEscape(u.getHost()) + "\", "//
+                        + "\"port\":" + u.getPort() + ", \"proto\":\"" + JSONUtils.jsonEscape(u.getProtocol()) + "\"}");
+                ps.println();
+                for (String hostip : iplist) {
+                    ps.println("event: hostip");
+                    ps.println("data: {");
+                    ps.println("data: \"domain\": \"" + JSONUtils.jsonEscape(u.getHost()) + "\",");
+                    ps.println("data: \"port\": \"" + JSONUtils.jsonEscape(u.getProtocol()) + "-" + JSONUtils.jsonEscape(Integer.toString(u.getPort())) + "\",");
+                    ps.println("data: \"ip\": \"" + JSONUtils.jsonEscape(hostip) + "\"");
+                    ps.println("data: }");
+                    ps.println();
+                }
+
+                ps.println("event: eof");
+                ps.println("data: {");
+                ps.println("data: msg: \"IP lookup completed.\"");
+                ps.println("data: }");
+                ps.println();
+                return;
+            } else if ( !iplist.contains(ip)) {
+                ps.println("event: eof");
+                ps.println("data: {");
+                ps.println("data: msg: \"Host not found at this address.\"");
+                ps.println("data: }");
+                ps.println();
                 return;
             }
+
+            TestingSession to;
+            {
+                boolean observingOnly = false;
+
+                synchronized (cacheTestSession) {
+                    String lookupKey = ip + "#" + u.getHost() + ":" + u.getProtocol() + "-" + u.getPort();
+
+                    to = cacheTestSession.get(lookupKey);
+                    if (to == null) {
+                        to = new TestingSession(u.getHost(), ip, u.getPort(), u.getProtocol());
+                        cacheTestSession.put(lookupKey, to);
+                    } else {
+                        observingOnly = true;
+                    }
+                }
+
+                to.attach(ps);
+
+                if (observingOnly) {
+                    to.waitForCompletion();
+                    return;
+                }
+            }
+
+            to.performTest();
         }
 
-        to.performTest();
     }
 
 }
